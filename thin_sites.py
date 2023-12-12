85775453d666a7dc1654a5f042aefe7538120cbb
Stephen Schaffner <sfs@broadinstitute.org>
2023-12-12 09:45:16 -0500

    modify shebang

--- thin_sites.py
+++ thin_sites.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 from collections import Counter
 import sys
--- vcf2hmm.py
+++ vcf2hmm.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 # Script to output genotype data from VCF file formatted for hmmIBD
 
